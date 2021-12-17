37f4a7b1a3d682c13714e582f5d7b5272c8034d6
Ctaaffe <93030149+Ctaaffe@users.noreply.github.com>
2021-12-17 15:27:00 +0100

    Update exemple_div_reading.py

--- exemple_div_reading.py
+++ exemple_div_reading.py
@@ -11,19 +11,3 @@ tei_doc = 'file.xml'
 with open(tei_doc, 'r', encoding='utf-8') as tei:
     soup = BeautifulSoup(tei, 'lxml')  
 div=soup.div.getText(separator=' ', strip=True)
-sentences: List[str] = [div]
-
- 
-# if you havent downloaded the model change to "True"
-do_download = False
-if do_download:
-    for dl in download("grc"):
-        x = 1
-
-
-model_name = "grc"
-tagger = get_tagger(model_name, batch_size=256, device="cpu", model_path=None
-from pie_extended.models.lasla.imports import get_iterator_and_processor
-for sentence_group in sentences:
-    iterator, processor = get_iterator_and_processor()
-    print(tagger.tag_str(sentence_group, iterator=iterator, processor=processor) )
