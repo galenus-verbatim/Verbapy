4ec5199f9d306d23d1e7874b2b3c57dd7db27365
Ctaaffe <93030149+Ctaaffe@users.noreply.github.com>
2021-12-28 11:13:32 +0100

    Update lemma_iterated.py

--- lemma_iterated.py
+++ lemma_iterated.py
@@ -1,20 +1,37 @@
-from cltk import NLP
+#import all necessary modules
+import os
+from bs4 import BeautifulSoup
+from xml.etree import ElementTree
+from lxml import etree
+from typing import List
+from pie_extended.cli.utils import get_tagger, get_model, download
+from pie_extended.models.grc.imports import get_iterator_and_processor
 from bs4 import BeautifulSoup
 import os
 from os.path import dirname, join
+import csv
 
-directory=("TEI") # location of XML files on local drive
+directory=("test") # location of Epidoc files on the drive
 
 for infile in os.listdir(directory):
     filename=join(directory, infile)
     indata=open(filename,"r", encoding="utf-8", errors="ignore") 
     contents = indata.read()
+    name="" #using beatiful soup or other to extract the xml value title or chapter title
     soup = BeautifulSoup(contents,'xml') 
-    df2=soup.div.getText(separator='\n', strip=True)#get the text from the div tag
-    cltk_nlp_grc = NLP(language="grc")
-    %time cltk_doc_grc = cltk_nlp_grc.analyze(text=df2)
-    df2=cltk_doc_grc.stanza_doc.to_dict()
-    print(df2)
-
-#Problems with pie-extended need to replace cltk lemmas with that of pie 
-#Other problem the saving, files perfectly ordered with an id but in what format and how to save them?
+    text_data=soup.div.getText(separator='\n', strip=True) #get the text from the div tag
+    sentences: List[str] = [text_data]
+    model_name = "grc"
+    tagger = get_tagger(model_name, batch_size=256, device="cpu", model_path=None)
+    dictionary=list()
+    for sentence_group in sentences:
+        iterator, processor = get_iterator_and_processor()
+        for form in tagger.tag_str(sentence_group, iterator=iterator, processor=processor):
+            dictionary.append({"form": form['form'], 'lemma': form["lemma"], 'pos': form["pos"]})
+    keys = dictionary[0].keys()
+    a_file = open(f"{name}.csv", "w", encoding='utf-8')
+    dict_writer = csv.DictWriter(a_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(dictionary)
+    a_file.close()
+    
